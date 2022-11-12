0c08c3432b184aeed27ac9c3ba7bf75d91ae5400
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-11-13 00:03:51 +0530

    another method github actions

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+echo 'run application_start.sh: ' >> /home/ec2-user/prod-actions/deploy.log
 
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
-#start our node app in the background
-pm2 start app.js --name "express-app"
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/prod-actions/deploy.log
+pm2 restart nodejs-express-app >> /home/ec2-user/prod-actions/deploy.log
