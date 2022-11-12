caf704fb5c5b1ea79df401e22189590e86499a1e
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-11-12 19:40:02 +0530

    update stop script

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers if they are running"
-pkill node || echo "No node servers running"
+if pgrep node; then pkill node; fi
